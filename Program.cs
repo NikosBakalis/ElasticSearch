66498ade8469d1ea7b22d9fbfccbafe22ff006b3
NikosBakalis <bakalis.nikolas@gmail.com>
2020-10-25 16:52:44 +0200

    Small changes.

--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ namespace ElasticSearch
             // The path of the .csv file.
             var filePath = "../../../data/movies.csv";
 
+            // The name of the index.
+            var indexName = "movies";
+
             // Transforms .csv file to Movies class.
             var csvToMovies = Linker.CSVToMovies(filePath);
 
@@ -20,19 +23,19 @@ namespace ElasticSearch
             var elasticSearchClient = new ElasticClient();
 
             //  Creates the Elastic Search Client.
-            //var responce = await elasticSearchClient.Indices.CreateAsync("movies");
+            //var response = await elasticSearchClient.Indices.CreateAsync(indexName);
 
             // Deletes the elastic search Index.
-            //var delete = await elasticSearchClient.Indices.DeleteAsync("movies");
+            //var delete = await elasticSearchClient.Indices.DeleteAsync(indexName);
 
             // Indexes the Elastic Search Index.
-            //var indexResponce = await elasticSearchClient.BulkAsync(x => x.Index("movies").IndexMany(csvToMovies));
+            //var indexResponce = await elasticSearchClient.BulkAsync(x => x.Index(indexName).IndexMany(csvToMovies));
 
             // Searches all the data of a specific index.
-            var searchResponce = await elasticSearchClient.SearchAsync<Movies>(x => x.Query(y => y.MatchAll()).Index("movies").Size(10000));
+            var searchResponce = await elasticSearchClient.SearchAsync<Movies>(x => x.Query(y => y.MatchAll()).Index(indexName).Size(10000));
 
             // Searches all the data of a specific index matching a string.
-            var searchStringResponce = await elasticSearchClient.SearchAsync<Movies>(x => x.Query(y => y.Match(z => z.Field(a => a.title).Query("(1995)"))).Index("movies").Size(10000));
+            var searchStringResponce = await elasticSearchClient.SearchAsync<Movies>(x => x.Query(y => y.Match(z => z.Field(a => a.title).Query("(1995)"))).Index(indexName).Size(10000));
             //var searchStringResponce = await elasticSearchClient.SearchAsync<Movies>(x => x.Query(y => y.Match(z => z.Field(a => a.title).Query("(1995)"))).Index("movies").Size(10000));
         }
     }
